20e058363bd1836e434440c86a8ac6b3832aa191
Eternal-Monke <ali.allk@icloud.com>
2025-05-22 19:07:44 +1000

    started implementation of pll in rust

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+
+use std::process::Command;
+use std::env;
+use std::path::Path;
+
 fn main() {
-    println!("Hello, world!");
+
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 3 {
+        eprintln!("Usage: {} <input_video> <output_audio>", args[0]);
+        std::process::exit(1);
+    }
+
+    let input = &args[1];
+    let output = &args[2];
+
+    if !Path::new(input).exists() {
+        eprintln!("Error: input file '{}' does not exist.", input);
+        std::process::exit(1);
+    }
+
+    let status = Command::new("ffmpeg").args(["-i", input, "-q:a", "0", "-map", "a", output,]).status();
+    
+    match status {
+        Ok(_) => {},
+        Err(error) => {
+            println!("Failed to start ffmpeg error encountered is {}", error);
+            std::process::exit(1);
+        }
+    }
+
+
 }
